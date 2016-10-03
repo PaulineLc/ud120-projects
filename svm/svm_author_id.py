3846810f98fc25148a86cf95e1708cfe12120d71
Pauline Lallinec <12251066@ucdconnect.ie>
2016-10-03 15:23:37 +0100

    finished SVM mini-project

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,37 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+clf = SVC(kernel="rbf", C=10000.0)
+
+#Uncomment to reduce the number of training data (improves runtime)
+#features_train = features_train[:len(features_train)/100] 
+#labels_train = labels_train[:len(labels_train)/100] 
+
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time: " + str(round(time()-t0, 3)) + "s"
+
+t1  = time()
+pred = clf.predict(features_test)
+print "predicting time: " + str(round(time()-t1, 3)) + "s"
+
+print ""
+print "accuracy:"
+
+print(accuracy_score(labels_test, pred))
+
+print ""
+for myindex in [10, 26, 50]:
+    if pred[myindex] == 0:
+        print "Sara"
+    else:
+        print "Chris"
+
+print""
+print("Number of mails from Chris:", list(pred).count(1))
 #########################################################
 
 
