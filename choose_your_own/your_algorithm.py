ee48fc5ff4c8fd0f5a3c492b4678ae83b248815d
Pauline Lallinec <PaulineLc@users.noreply.github.com>
2017-01-11 13:49:41 +0000

    mini-project about the Enron dataset finished

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -32,11 +32,15 @@ plt.show()
 ### visualization code (prettyPicture) to show you the decision boundary
 
 
+from sklearn.ensemble import AdaBoostClassifier
 
+clf = AdaBoostClassifier()
+clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
 
+from sklearn.metrics import accuracy_score
 
-
-
+print(accuracy_score(pred, labels_test))
 
 try:
     prettyPicture(clf, features_test, labels_test)
--- datasets_questions/explore_enron_data.py
+++ datasets_questions/explore_enron_data.py
@@ -19,4 +19,52 @@ import pickle
 
 enron_data = pickle.load(open("../final_project/final_project_dataset.pkl", "r"))
 
+print(len(enron_data))
+print(len(enron_data[enron_data.keys()[0]]))
 
+num_poi = 0
+
+for person in enron_data:
+    if enron_data[person]['poi']:
+        num_poi += 1
+
+print(num_poi)
+
+print(enron_data["Prentice james".upper()]["total_stock_value"])
+
+print(enron_data["Colwell Wesley".upper()]["from_this_person_to_poi"])
+
+print(enron_data["Skilling Jeffrey K".upper()]["exercised_stock_options"])
+
+print("total payments")
+print(enron_data["skilling jeffrey k".upper()]["total_payments"])
+print(enron_data["lay kenneth l".upper()]["total_payments"])
+print(enron_data["fastow andrew s".upper()]["total_payments"])
+
+num_valid_salary = 0
+num_valid_email = 0
+
+for person in enron_data:
+    if str(enron_data[person]["salary"]) != "NaN":
+        num_valid_salary += 1
+    if str(enron_data[person]["email_address"]) != "NaN":
+        num_valid_email += 1
+
+print("Num valid salary:", num_valid_salary)
+print("Num valid email:", num_valid_email)
+
+num_invalid_total_payment = 0
+for person in enron_data:
+    if str(enron_data[person]["total_payments"]) == "NaN":
+        num_invalid_total_payment += 1
+
+print("Invalid total payment:", num_invalid_total_payment)
+print("%:", float(num_invalid_total_payment) / len(enron_data.keys()))
+
+num_invalid_total_payment_poi = 0
+
+for person in enron_data:
+    if enron_data[person]["poi"] and str(enron_data[person]["total_payments"]) == "NaN":
+        num_invalid_total_payment_poi += 1
+
+print("Invalid total payments for poi %:", float(num_invalid_total_payment_poi) / num_poi)
